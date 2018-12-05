332b82caf62890849a5b58543c95123164ad3466
mtolkoff <mtolkoff@gmail.com>
2018-12-05 18:15:13 -0500

    adds extra files that weren't there before

---
+++ addNamesLoop.py
@@ -0,0 +1,25 @@
+otuToNames_file = open('99_otu_taxonomy.txt', 'r')
+otuToName = otuToNames_file.read()
+otuToName = otuToName.split('\n')
+
+otuToNameSplit = []
+for i in range(len(otuToName) - 1):
+     otuToNameSplit.append(otuToName[i].split('\t'))
+
+#otuToNameSplit = np.transpose(otuToNameSplit)
+
+dataNames = []
+for i in range(5000):
+    data_test.iloc[i, 0] = np.transpose(otuToNameSplit)[1][list(np.transpose(otuToNameSplit)[0]).index(str(data_test.iloc[i, 0]))]
+
+for i in range(5000):
+    count = 6
+    isFound = False
+    treeLine = data_test.iloc[i, 0].split("; ")
+    while not isFound:
+        if len(treeLine[count]) <= 4:
+                count-= 1
+        else:
+                data_test.iloc[i, 0] = treeLine[count].rstrip('\n')
+                isFound = True
+
