1ec8bd580f8ae5e3b8b4c7b0ff9f44dcf94d5816
mtolkoff <mtolkoff@gmail.com>
2019-03-14 16:39:46 -0400

    edits code which adds names to sparse file, should make it more conducive to running rather than simply a collection of useful functions

--- addNamesLoop.py
+++ addNamesLoop.py
@@ -1,29 +1,29 @@
-#read file
-otuToNames_file = open('99_otu_taxonomy.txt', 'r')
-otuToName = otuToNames_file.read()
-otuToName = otuToName.split('\n')
+import pandas as pd
 
-#splits taxonomic groupings from OTU numbers
+otuToNames_file = open('../Downloads/99_otu_taxonomy.txt', 'r')
+data_test = pd.read_csv('picrust_midSize', index_col = 0)
+otuToName = otuToNames_file.read()
+
 otuToNameSplit = []
 for i in range(len(otuToName) - 1):
      otuToNameSplit.append(otuToName[i].split('\t'))
 
 #otuToNameSplit = np.transpose(otuToNameSplit)
 
-#Replaces OTU number with full taxonomic information
 dataNames = []
 for i in range(5000):
-    data_test.iloc[i, 0] = np.transpose(otuToNameSplit)[1][list(np.transpose(otuToNameSplit)[0]).index(str(data_test.iloc[i, 0]))]
+    dataNames.append(np.transpose(otuToNameSplit)[1][list(np.transpose(otuToNameSplit)[0]).index(str(data_test.index[i]))])
 
-#replaces full taxonomic information with just name of lowest level group
+finalName = []
 for i in range(5000):
     count = 6
     isFound = False
-    treeLine = data_test.iloc[i, 0].split("; ")
+    treeLine = dataNames[i].split("; ")
     while not isFound:
         if len(treeLine[count]) <= 4:
                 count-= 1
         else:
-                data_test.iloc[i, 0] = treeLine[count].rstrip('\n')
+                finalName.append(treeLine[count].rstrip('\n')
                 isFound = True
 
+data_test['taxa_name'] = finalName
